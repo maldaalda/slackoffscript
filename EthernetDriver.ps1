03e93d46f73928b8bc4716ed2d2a0d9251daf576
Alexandr <95874421+maldaalda@users.noreply.github.com>
2026-04-16 11:55:12 +0200

    Fix progress message typo and increment logic

--- EthernetDriver.ps1
+++ EthernetDriver.ps1
@@ -3,5 +3,5 @@ while($true) {
     Write-Progress -Activity "Waiting for server response..." -Status "Downloading...." -PercentComplete $i;
     Start-Sleep -Milliseconds 500
     Write-Progress -Activity "Donwnloading..." -Status "Downloading in progres..." -PercentComplete $i;
-    
+    $i = +1
 }
