233bdff70742106b74e00376e7e30e6d4db1cd87
Alexandr <95874421+maldaalda@users.noreply.github.com>
2026-04-16 11:56:16 +0200

    Update sleep duration and fix progress message
    Increased sleep duration to 5000 milliseconds and corrected progress message.

--- EthernetDriver.ps1
+++ EthernetDriver.ps1
@@ -1,7 +1,7 @@
 while($true) {
     $i = 1
-    Write-Progress -Activity "Waiting for server response..." -Status "Downloading...." -PercentComplete $i;
-    Start-Sleep -Milliseconds 500
+    #Write-Progress -Activity "Waiting for server response..." -Status "Downloading...." -PercentComplete $i;
+    Start-Sleep -Milliseconds 5000
     Write-Progress -Activity "Donwnloading..." -Status "Downloading in progres..." -PercentComplete $i;
     $i = +1
 }
